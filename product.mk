db5e6dc8ff37f892e3be4e487d9a6b35c6a73827
Arian <arian.kulmer@web.de>
2020-05-07 12:46:37 +0200

    18.1 GMS, here we go!
    Change-Id: I257bb9220d10ef2cfcd30cddc7464ce84e759937

---
+++ product.mk
@@ -0,0 +1,9 @@
+# Google Apps
+WITH_GMS := true
+$(call inherit-product, vendor/gms/products/gms.mk)
+
+# OTA
+PRODUCT_PACKAGES += UpdaterOverlay
+
+# Translations
+PRODUCT_PACKAGE_OVERLAYS += vendor/translations/overlay
