1dfb2ad1b06a16446f7fc8ed990d466d6734e593
Arian <arian.kulmer@web.de>
2020-05-07 12:46:37 +0200

    19.1, here we go!
    Change-Id: I257bb9220d10ef2cfcd30cddc7464ce84e759937

---
+++ product.mk
@@ -0,0 +1,11 @@
+#
+# Copyright (C) 2022 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# OTA
+PRODUCT_PACKAGES += UpdaterOverlay
+
+# Translations
+PRODUCT_PACKAGE_OVERLAYS += vendor/translations/overlay
