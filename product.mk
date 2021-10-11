0a4efa5e7eac7cae649a47a16887d91fb0cde27d
AdarshGrewal <adarshgrewal@gmail.com>
2021-10-11 23:00:43 +0530

    extra: import Google Sans font for the KeyguardClockSwitch
     * The KeyguardClockSwitch looks for "@font/clock" which is
     google_sans_clock Google ships this on Pixels but decided to leave it
     out for aosp.

--- product.mk
+++ product.mk
@@ -8,6 +8,10 @@
 WITH_GMS := true
 $(call inherit-product, vendor/gms/products/gms.mk)
 
+# Fonts
+PRODUCT_ENFORCE_RRO_EXCLUDED_OVERLAYS += vendor/extra/overlay
+PRODUCT_PACKAGE_OVERLAYS += vendor/extra/overlay
+
 # OTA
 PRODUCT_PACKAGES += UpdaterOverlay
 
