5a6779feba3189c9d9cb2949891bd01e127ddd4f
Arian <arian.kulmer@web.de>
2023-07-09 21:42:28 +0200

    Include ih8sn

--- product.mk
+++ product.mk
@@ -8,6 +8,13 @@
 PRODUCT_ENFORCE_RRO_EXCLUDED_OVERLAYS += vendor/extra/overlay
 PRODUCT_PACKAGE_OVERLAYS += vendor/extra/overlay
 
+# ih8sn
+PRODUCT_PACKAGES += \
+    ih8sn
+
+PRODUCT_COPY_FILES += \
+    vendor/extra/ih8sn.conf:$(TARGET_COPY_OUT_SYSTEM)/etc/ih8sn.conf
+
 # OTA
 PRODUCT_PACKAGES += UpdaterOverlay
 
