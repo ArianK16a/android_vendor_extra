7d6508ffecb10b75beb24fefc27b9eb0c582d765
Arian <arian.kulmer@web.de>
2024-07-24 23:53:21 +0200

    Revert "Include ih8sn"
    This reverts commit 5a6779feba3189c9d9cb2949891bd01e127ddd4f.

--- product.mk
+++ product.mk
@@ -4,13 +4,6 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
-# ih8sn
-PRODUCT_PACKAGES += \
-    ih8sn
-
-PRODUCT_COPY_FILES += \
-    vendor/extra/ih8sn.conf:$(TARGET_COPY_OUT_SYSTEM)/etc/ih8sn.conf
-
 # MiuiCamera
 $(call inherit-product-if-exists, device/xiaomi/miuicamera-$(shell echo -n $(TARGET_PRODUCT) | sed -e 's/^[a-z]*_//g')/device.mk)
 
